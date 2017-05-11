5424e4195ba9352d56f4710d9a7598d0409cdeed
Luigi A. A. Sansonetti <luigi@sansonetti.ch>
2017-05-11 09:06:19 +0200

    Initial Commit

---
+++ Informant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Informant
+//
+//  Created by Luigi Sansonetti on 11.05.17.
+//  Copyright © 2017 Luigi Sansonetti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
